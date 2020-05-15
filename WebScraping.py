3fbba04e2fb7c9d6715807b87d337e59782734f4
Mrugapphan Kannan <52872043+kannan-mrugapphan@users.noreply.github.com>
2020-05-15 18:55:09 -0500

    Update WebScraping.py

--- WebScraping.py
+++ WebScraping.py
@@ -25,7 +25,7 @@ def formatDate(date):
     datetimeObject = datetime.strptime(date, '%d %B %Y')
     return datetimeObject.strftime('%Y-%m-%dT%H:%M:%S.%f%z')
 
-def buildCVE(vulnerablity):
+def buildCVE(vulnerablity, idColumn, descriptionColumn):
     timestamp = datetime.utcnow().isoformat() #current timestamp in iso format
     currentCVE['Timestamp'] = timestamp
 
@@ -43,18 +43,18 @@ def buildCVE(vulnerablity):
 
     #print(vulnerablityDetails.prettify())
     #cve id is the 5th column in vulnerablity table
-    id = vulnerablity.findAll('td')[4].text.replace(u'\xa0', u'').replace(u'\n', u'')
+    id = vulnerablity.findAll('td')[idColumn].text.replace(u'\xa0', u'').replace(u'\n', u'')
     currentCVE['ID'] = id
 
     url = 'https://helpx.adobe.com/security/products/magento/apsb20-02.html'
     currentCVE['URL'] = url
 
     #product name is obtained from page description class
-    name = soup.find('div', class_ = 'page-description').text.strip().split('|')[0].split()[-1]
+    name = soup.find('div', class_ = 'page-description').text.strip().split('|')[0].split('for')[1]
     currentCVE['Name'] = name
 
     #category is the 1st column in vulnerablity table
-    description = vulnerablity.findAll('td')[0].text
+    description = vulnerablity.findAll('td')[descriptionColumn].text
     currentCVE['Description'] = description.replace(u'\xa0', u'').replace(u'\n', u'')
 
     #info about each affected version is dumped into cpe list
@@ -71,18 +71,24 @@ def buildCVE(vulnerablity):
 
     currentCVE['CPEs'] = {}
     currentCVE['CPEs']['CPE_List'] = cpeList
-    CVEs.append(currentCVE)
+    CVEs.append(currentCVE.copy())
     return
 
 def buildResult():
     #populate the CVE info starting from the vulnerablity table
     tables = soup.findAll('div', class_ = 'table parbase section')
     vulnerablityDetails = tables[3]
+    columnNames = vulnerablityDetails.findAll('tr')[0].findAll('td')
+    for i in range(len(columnNames)):
+        if("CVE Number" in columnNames[i].text):
+            idColumn = i
+        if("Vulnerability Category" in columnNames[i].text):
+            descriptionColumn = i 
     vulnerablityDetails = vulnerablityDetails.findAll('tr')[1:]
     for vulnerablity in vulnerablityDetails:
-        buildCVE(vulnerablity)
+        buildCVE(vulnerablity, idColumn, descriptionColumn)
 
-    result['Source'] = URL.split('//')[-1].split('/')[0] #source is obtained from domain name of url
+    result['Source'] = URL.split('//')[-1].split('/')[0].split('.')[-2] #source is obtained from domain name of url
     result['Type'] = 'Vendor'
     result['CVEs'] = CVEs
     return
@@ -107,5 +113,6 @@ for url in urlList:
     buildResult()
 
     output = json.dumps(result, indent = 4)
+    #print(output)
     with open(outputFileName, 'w') as f:
         f.write(output)
