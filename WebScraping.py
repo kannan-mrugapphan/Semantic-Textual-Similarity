a64eed7b777d4bb7d0ec607dcd4a2e1a9a062b47
Mrugapphan Kannan <52872043+kannan-mrugapphan@users.noreply.github.com>
2020-05-15 23:04:38 -0500

    Update WebScraping.py

--- WebScraping.py
+++ WebScraping.py
@@ -25,7 +25,7 @@ def formatDate(date):
     datetimeObject = datetime.strptime(date, '%d %B %Y')
     return datetimeObject.strftime('%Y-%m-%dT%H:%M:%S.%f%z')
 
-def buildCVE(vulnerablity, idColumn, descriptionColumn):
+def buildCVE(vulnerablity, idColumn, descriptionColumn, affectedVersion):
     timestamp = datetime.utcnow().isoformat() #current timestamp in iso format
     currentCVE['Timestamp'] = timestamp
 
@@ -58,35 +58,65 @@ def buildCVE(vulnerablity, idColumn, descriptionColumn):
     currentCVE['Description'] = description.replace(u'\xa0', u'').replace(u'\n', u'')
 
     #info about each affected version is dumped into cpe list
-    cpeList = []
-    affectedVersions = tables[1].findAll('tr')[1:]
-    for version in affectedVersions:
-        version = version.findAll('td')
-        current = {}
-        current['Vendor'] = version[0].text.split()[0]
-        current['Product'] = version[0].text.replace(u'\xa0', u'').replace(u'\n', u'')
-        current['Category'] = 'a'
-        current['VersionEndIncluding'] = version[1].text.split()[0].replace(u'\xa0', u'').replace(u'\n', u'')
-        cpeList.append(current)
-
+    cpeList = buildCPEList(affectedVersion, tables)
     currentCVE['CPEs'] = {}
     currentCVE['CPEs']['CPE_List'] = cpeList
     CVEs.append(currentCVE.copy())
     return
 
+def buildCPEList(affectedVersion, tables):
+    cpeList = []
+    if(affectedVersion == None):
+        columnNames = tables[1].findAll('tr')[0].text.strip().split('\n')
+        versionId = 0
+        productId = 0
+        for i in range(len(columnNames)):
+            if("Version" in columnNames[i]):
+                versionId = i
+            if("Product" in columnNames[i]):
+                productId = i
+        affectedVersions = tables[1].findAll('tr')[1:]
+        for version in affectedVersions:
+            version = version.findAll('td')
+            current = {}
+            current['Vendor'] = version[productId].text.split()[0]
+            current['Product'] = version[productId].text.replace(u'\xa0', u'').replace(u'\n', u'')
+            current['Category'] = 'a'
+            current['VersionEndIncluding'] = version[versionId].text.split()[0].replace(u'\xa0', u'').replace(u'\n', u'')
+            cpeList.append(current)
+    else:
+        category = tables[3].findAll('tr')[1:]
+        for row in category:
+            version = row.findAll('td')[affectedVersion].text.strip().replace(u'\xa0', u'').replace(u' ', u'').split('\n')
+            current = {}
+            affectedVersions = tables[1].findAll('tr')[1:]
+            for product in affectedVersions:
+                product = product.findAll('td')
+                current = {}
+                current['Vendor'] = product[0].text.split()[0]
+                current['Product'] = product[0].text.replace(u'\xa0', u'').replace(u'\n', u'')
+            current['Category'] = 'a'
+            current['VersionStartIncluding'] = version[0]
+            current['VersionEndIncluding'] = version[-1]
+            cpeList.append(current)
+    return cpeList
+        
 def buildResult():
     #populate the CVE info starting from the vulnerablity table
     tables = soup.findAll('div', class_ = 'table parbase section')
     vulnerablityDetails = tables[3]
     columnNames = vulnerablityDetails.findAll('tr')[0].findAll('td')
+    affectedVersion = None
     for i in range(len(columnNames)):
         if("CVE Number" in columnNames[i].text):
             idColumn = i
         if("Vulnerability Category" in columnNames[i].text):
-            descriptionColumn = i 
+            descriptionColumn = i
+        if("Affected Versions" in columnNames[i].text):
+            affectedVersion = i
     vulnerablityDetails = vulnerablityDetails.findAll('tr')[1:]
     for vulnerablity in vulnerablityDetails:
-        buildCVE(vulnerablity, idColumn, descriptionColumn)
+        buildCVE(vulnerablity, idColumn, descriptionColumn, affectedVersion)
 
     result['Source'] = URL.split('//')[-1].split('/')[0].split('.')[-2] #source is obtained from domain name of url
     result['Type'] = 'Vendor'
@@ -113,6 +143,6 @@ for url in urlList:
     buildResult()
 
     output = json.dumps(result, indent = 4)
-    #print(output)
+    print(output)
     with open(outputFileName, 'w') as f:
         f.write(output)
